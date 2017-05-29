f9942705f33fece82ce5a72f1341d5c89b7a2999
al.lastor <al.lastor@bk.ru>
2017-05-29 18:39:17 +0300

    v.0.0.1

--- file_write.lua
+++
@@ -1,6 +0,0 @@
-function file_write (f, txt)
-    local file = io.open (f, "w")
-    file:write(txt)
-    file:flush()
-    file:close()
-end
--- lua_init.lua
+++ lua_init.lua
@@ -1,5 +1,11 @@
 #!/usr/bin/lua5.2
-dofile ("./file_write.lua")
+
+function file_write (f, txt)
+    local file = io.open (f, "w")
+    file:write(txt)
+    file:flush()
+    file:close()
+end
 
 file_write ("./version.lua", "v1 = 0 ; v2 = 0 ; v3 = 0")
 file_write ("./log", "#0.0.1\n    Инициализация проекта.")
